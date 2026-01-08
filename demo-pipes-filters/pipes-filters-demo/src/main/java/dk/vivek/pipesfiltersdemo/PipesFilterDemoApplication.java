f126e580f5aa0c75b67326070d8d952cc5e16001
V <71701790+VivekNagra@users.noreply.github.com>
2026-01-08 21:53:53 +0100

    updated main README.md.

---
+++ demo-pipes-filters/pipes-filters-demo/src/main/java/dk/vivek/pipesfiltersdemo/PipesFilterDemoApplication.java
@@ -0,0 +1,12 @@
+package dk.vivek.pipesfiltersdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipesFiltersDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipesFiltersDemoApplication.class, args);
+    }
+}
